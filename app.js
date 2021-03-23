1567061042139f0fe561ec3428a56114c0f34ee1
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2021-03-23 23:30:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(window.innerHeight)
+const headerBtn = document.getElementById('headerBtn');
+const nav = document.getElementById('nav');
+
+let navState = true;
+
+headerBtn.addEventListener('click', ()=>{
+    if(navState){
+        nav.style.transform = "scaleY(1)";
+        navState = false;
+    }else{
+        nav.style.transform = "scaleY(0)";
+        navState = true;
+    }
+})
